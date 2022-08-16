887192dbeb5544d40cee93ddeec5728eb40886d4
naikpriti <priti.naik@lexisnexisrisk.com>
2022-08-16 11:07:48 +0530

    add

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "azurerm_servicebus_namespace" "busNamespace" {
-  name                = var.services_bus_namespace_name
+  for_each      =var.services_bus_namespace_name
+  name                = each.key
   location            = var.location
   resource_group_name = var.resource_group
   sku                 = "Standard"
@@ -12,6 +13,8 @@ resource "azurerm_servicebus_queue" "app1MessagesQueue" {
   for_each            = var.messages_queue_name
   name                = each.key
   #resource_group_name = var.resource_group
-  namespace_id      = each.services_bus_namespace_name
-  max_size_in_megabytes = each.value.max_size   
+  #namespace_id      = "/subscriptions/${var.subscription_id}/resourceGroups/${var.resource_group}/providers/Microsoft.ServiceBus/namespaces/"
+  namespace_id = each.value.services_bus_namespace_name
+  max_size_in_megabytes = var.max_size_in_megabytes   
 }
+
--- variable.tf
+++ variable.tf
@@ -7,9 +7,16 @@ variable "resource_group" {
 variable "messages_queue_name" {
   description = "messagesqueue_name"
   type = map(object({
+    services_bus_namespace_name = any
     max_size = string
   }))
   default = {}
+}
+
+
+variable "services_bus_namespace_name" {
+  description = "services_bus_namespace_name"
+  type = any
 }
 
 variable "location" {
@@ -23,16 +30,25 @@ variable "tags" {
   default     = {}
 }
 
+variable "max_size_in_megabytes" {
+  description = "max_size_in_megabytes"
+  type        = any
+  default     = 1024
+}
+
+variable "subscription_id" {
+  description = "max_size_in_megabytes"
+  type        = any
+}
+
+
 variable "names" {
   description = "A map of key value pairs to add to all resources"
   type        = map(string)
   default     = {}
 }
 
-variable "services_bus_namespace_name" {
-  description = "service bus namespace name"
-  type        = any
-}
+
 
 variable "subscription_name" {
   description = "subscription_name"
